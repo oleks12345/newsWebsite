e4f2615ec317b555eb94c5c78b1f90f2cf6d55fc
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-13 18:51:12 +0200

    create NavBar structure

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,10 +1,50 @@
 import React from 'react';
 
+import IconSearch from 'assets/images/iconSearch.svg';
+import {
+   StyledHeader,
+   NavList,
+   NavItem,
+   NavLink,
+   HeaderBrand,
+   SearchInput,
+   StyledNav,
+   NavSearch,
+   SearchButton,
+   SearchImg,
+} from './NavBar_styles';
+import { useInput } from '../../utils/customHooks';
+
 const NavBar = () => {
+   const searchInput = useInput( '' );
+
    return (
-      <nav>
-         <div />
-      </nav>
+      <StyledHeader>
+         <HeaderBrand>News</HeaderBrand>
+         <StyledNav>
+            <NavSearch>
+               <SearchInput { ...searchInput }></SearchInput>
+               <SearchButton>
+                  { ' ' }
+                  <SearchImg src={ IconSearch } alt="" />{ ' ' }
+               </SearchButton>
+            </NavSearch>
+            <NavList>
+               <NavItem>
+                  <NavLink href="#home">Link</NavLink>
+               </NavItem>
+               <NavItem>
+                  <NavLink href="#home">Link2</NavLink>
+               </NavItem>
+               <NavItem>
+                  <NavLink href="#home">Link3</NavLink>
+               </NavItem>
+               <NavItem>
+                  <NavLink href="#home">Link4</NavLink>
+               </NavItem>
+            </NavList>
+         </StyledNav>
+      </StyledHeader>
    );
 };
 
