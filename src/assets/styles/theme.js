b7e5aeb82034939ba9ee6b2829b1c7eff21cb097
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-14 11:08:06 +0200

    add small media querry to theme

--- src/assets/styles/theme.js
+++ src/assets/styles/theme.js
@@ -20,6 +20,7 @@ export const theme = {
    },
    maxWidth: '1080px',
    mdq: {
+      sm: '@media (min-width: 640px)',
       md: '@media (min-width: 768px)',
       lg: '@media (min-width: 1024px)',
       xl: '@media(min-width: 1440px)',
