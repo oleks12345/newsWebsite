8a6fbffe1e781f7046f9f0c589f3a4d42c19108c
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-15 17:33:42 +0200

    add ArticlesLoading

--- src/components/Articles/Articles_styles.js
+++ src/components/Articles/Articles_styles.js
@@ -2,14 +2,22 @@ import styled from 'styled-components';
 
 export const ArticlesSection = styled.main`
    padding-top: 70px;
+   min-height: 100vh;
 `;
-
 export const ArticlesWrapper = styled.div`
    max-width: ${( { theme } ) => theme.maxWidth};
    margin: 0 auto;
    display: flex;
    flex-wrap: wrap;
    justify-content: space-between;
+`;
+
+export const ArticlesLoading = styled.div`
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   padding: 10%;
+   align-items: center;
 `;
 
 export const Article = styled.article`
