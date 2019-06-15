dbef2d5d25a0a54896363d1d32d766b243c566de
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-15 12:41:58 +0200

    create basic Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Footer = () => {
+   return (
+      <footer>
+         <div>
+            <p>
+               &copy; <a href="https://oleks.pl">oleks.pl</a>
+            </p>
+            <p>
+               powered by <a href="https://newsapi.org">NewsAPI.org</a>
+            </p>
+         </div>
+      </footer>
+   );
+};
+
+export default Footer;
