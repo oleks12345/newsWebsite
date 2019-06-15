0fbf4ad96fdc17df2e5e8d4b306acef42bdf3b8a
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-15 13:28:39 +0200

    change footer styles

--- src/components/Footer/Footer_styles.js
+++ src/components/Footer/Footer_styles.js
@@ -3,15 +3,24 @@ import styled from 'styled-components';
 export const StyledFooter = styled.footer`
    background-color: ${( { theme } ) => theme.secondary};
    color: ${( { theme } ) => theme.white};
-   font-size: ${( { theme } ) => theme.font.size.s};
+   font-size: ${( { theme } ) => theme.font.size.xs};
    display: flex;
    justify-content: center;
+   ${( { theme } ) => theme.mdq.sm} {
+      font-size: ${( { theme } ) => theme.font.size.s};
+   }
 `;
 
 export const FooterWrapper = styled.div`
    width: 100%;
    max-width: ${( { theme } ) => theme.maxWidth};
+   padding: 0.5em 0;
    display: flex;
-   align-items: space-around;
+   align-items: center;
    justify-content: space-around;
+
+   ${( { theme } ) => theme.mdq.md} {
+      justify-content: space-between;
+      padding: 0.5em 2em;
+   }
 `;
