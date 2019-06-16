badde0c684ba7e2c1f1e59b377d752bee08d7035
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-16 13:51:08 +0200

    add tests

--- src/components/Articles/Article/Article.test.js
+++ src/components/Articles/Article/Article.test.js
@@ -1,36 +1,77 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import 'jest-styled-components';
 
+import { mount } from 'enzyme';
 import Article from './Article';
-import { mountWithTheme } from '../../../__tests_utilities__/utilities';
+import { componentWithTheme } from '../../../__tests_utilities__/utilities';
 import { ArticleDate } from './Article_style';
 
-describe( '<Article/>', () => {
+describe( 'component <Article/>', () => {
+   it( 'renders without crushing', () => {
+      const div = document.createElement( 'div' );
+      ReactDOM.render(
+         componentWithTheme(
+            <Article
+               data={ {
+                  title: '',
+                  publishedAt: '',
+                  urlToImage: '',
+                  description: '',
+               } }
+            />
+         ),
+         div
+      );
+      ReactDOM.unmountComponentAtNode( div );
+   } );
+   it( 'renders with all data null ', () => {
+      const div = document.createElement( 'div' );
+      ReactDOM.render(
+         componentWithTheme(
+            <Article
+               data={ {
+                  title: null,
+                  publishedAt: null,
+                  urlToImage: null,
+                  description: null,
+               } }
+            />
+         ),
+         div
+      );
+      ReactDOM.unmountComponentAtNode( div );
+   } );
+
    it( 'renders without image', () => {
-      const article = mountWithTheme(
-         <Article
-            data={ {
-               title: '',
-               publishedAt: '',
-               urlToImage: null,
-               description: '',
-            } }
-         />
+      const article = mount(
+         componentWithTheme(
+            <Article
+               data={ {
+                  title: '',
+                  publishedAt: '',
+                  urlToImage: null,
+                  description: '',
+               } }
+            />
+         )
       );
       expect( article.find( 'img' ).prop( 'src' ) ).toContain( 'noImage.svg' );
    } );
 
    it( 'cuts description after 200 chars', () => {
-      const article = mountWithTheme(
-         <Article
-            data={ {
-               title: '',
-               publishedAt: '',
-               urlToImage: '',
-               description:
-                  'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae vero hic iusto eveniet libero, laborum consequatur? Nemo unde sunt inventore provident quae ipsam dolore, repudiandae, tempore possimus porro, distinctio cumque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae vero hic iusto eveniet libero, laborum consequatur? Nemo unde sunt inventore provident quae ipsam dolore, repudiandae, tempore possimus porro, distinctio cumque',
-            } }
-         />
+      const article = mount(
+         componentWithTheme(
+            <Article
+               data={ {
+                  title: '',
+                  publishedAt: '',
+                  urlToImage: '',
+                  description:
+                     'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae vero hic iusto eveniet libero, laborum consequatur? Nemo unde sunt inventore provident quae ipsam dolore, repudiandae, tempore possimus porro, distinctio cumque! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Molestiae vero hic iusto eveniet libero, laborum consequatur? Nemo unde sunt inventore provident quae ipsam dolore, repudiandae, tempore possimus porro, distinctio cumque',
+               } }
+            />
+         )
       );
 
       const description = article.find( 'p' ).text();
@@ -41,15 +82,17 @@ describe( '<Article/>', () => {
    } );
 
    it( 'works with wrong date', () => {
-      const article = mountWithTheme(
-         <Article
-            data={ {
-               title: '',
-               publishedAt: '0000000000',
-               urlToImage: '',
-               description: '',
-            } }
-         />
+      const article = mount(
+         componentWithTheme(
+            <Article
+               data={ {
+                  title: '',
+                  publishedAt: '0000000000',
+                  urlToImage: '',
+                  description: '',
+               } }
+            />
+         )
       );
 
       const date = article.find( ArticleDate ).text();
