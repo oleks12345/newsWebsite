7f81556b82ac473409bd3b9f584e29199d0bba47
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-16 13:51:00 +0200

    change test utilites

--- src/__tests_utilities__/utilities.js
+++ src/__tests_utilities__/utilities.js
@@ -1,14 +1,9 @@
 import React from 'react';
-import { shallow, mount } from 'enzyme';
 
 import { ThemeProvider } from 'styled-components';
 
 import { theme } from '../assets/styles/theme';
 
-export const shallowWithTheme = ( tree ) => {
-   return shallow( <ThemeProvider theme={ theme }>{ tree }</ThemeProvider> );
-};
-
-export const mountWithTheme = ( tree ) => {
-   return mount( <ThemeProvider theme={ theme }>{ tree }</ThemeProvider> );
+export const componentWithTheme = ( tree ) => {
+   return <ThemeProvider theme={ theme }>{ tree }</ThemeProvider>;
 };
