be76b1b9f552a68ec0474706780d5eeb37404fda
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-16 10:57:06 +0200

    create Article

---
+++ src/components/Articles/Article/Article.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import noImage from 'assets/images/noImage.svg';
+import {
+   StyledArticle,
+   ArticleImage,
+   ArticleLink,
+   ArticleDate,
+} from './Article_style';
+
+const Article = ( { data } ) => {
+   const cutDescriptionAtWhitespaceAfterNthLetter = (
+      description,
+      words = 200
+   ) => {
+      const reserve = words * 0.1;
+
+      if ( description.length > words + reserve ) {
+         for ( let i = words; i < description.length; i++ ) {
+            const currentChar = description[i];
+
+            if (
+               currentChar === ' ' ||
+               currentChar === ',' ||
+               currentChar === '.'
+            ) {
+               return description.slice( 0, i ) + '...';
+            }
+         }
+      }
+
+      return description;
+   };
+
+   const cutDateAtDays = ( date = '' ) => {
+      const tPosition = date.indexOf( 'T' );
+
+      if ( tPosition ) {
+         return date.slice( 0, tPosition );
+      }
+
+      return date;
+   };
+
+   return (
+      <StyledArticle>
+         <ArticleLink href={ data.url }>
+            <ArticleImage
+               src={ data.urlToImage || noImage }
+               alt="article image"
+            />
+            <h3>{ data.title }</h3>
+            <p>
+               { cutDescriptionAtWhitespaceAfterNthLetter( data.description ) }
+               <ArticleDate>{ cutDateAtDays( data.publishedAt ) }</ArticleDate>
+            </p>
+         </ArticleLink>
+      </StyledArticle>
+   );
+};
+
+Article.propTypes = {
+   data: PropTypes.shape().isRequired,
+};
+
+export default Article;
