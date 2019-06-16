ef165f4d41b96083157c715674dbe07029ab52f3
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-16 13:54:53 +0200

    add description to Index tests

--- src/pages/Index/Index.test.js
+++ src/pages/Index/Index.test.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { mount } from 'enzyme';
 import Index from './Index';
 
-it( 'renders without crashing', () => {
-   mount( <Index /> );
+describe( 'component <Index/>', () => {
+   it( 'renders without crashing', () => {
+      mount( <Index /> );
+   } );
 } );
