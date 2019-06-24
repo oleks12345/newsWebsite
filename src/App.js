08533723d0c58715eb8c154ff1ebc8113bde9ead
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-24 12:24:23 +0200

    add default route to App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import PageTemplate from './templates/PageTemplate/PageTemplate';
 import Index from './pages/Index/Index';
+import ErrorPage from './pages/ErrorPage/ErrorPage';
 
 const App = () => {
    return (
@@ -10,6 +11,7 @@ const App = () => {
          <PageTemplate>
             <Switch>
                <Route exact path="/" component={ Index } />
+               <Route component={ ErrorPage } />
             </Switch>
          </PageTemplate>
       </BrowserRouter>
