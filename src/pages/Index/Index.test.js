680926db85eb0bd82e003951dee545171364496f
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-06-27 11:34:39 +0200

    fix test

--- src/pages/Index/Index.test.js
+++ src/pages/Index/Index.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { mount } from 'enzyme';
+import { componentWithTheme } from '__tests_utilities__/utilities';
 import Index from './Index';
 
 describe( 'component <Index/>', () => {
    it( 'renders without crashing', () => {
-      mount( <Index /> );
+      mount( componentWithTheme( <Index /> ) );
    } );
 } );
