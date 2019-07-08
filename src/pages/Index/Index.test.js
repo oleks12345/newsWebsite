3f16b1fa8851feb221ec3e7ad4ac88b5b1738655
Aleksander Uniatowicz <oleks0709@gmail.com>
2019-07-08 10:34:20 +0200

    fix testing error

--- src/pages/Index/Index.test.js
+++ src/pages/Index/Index.test.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { mount } from 'enzyme';
 import { componentWithTheme } from '__tests_utilities__/utilities';
+import { StaticRouter as Router } from 'react-router-dom';
 import Index from './Index';
 
 describe( 'component <Index/>', () => {
    it( 'renders without crashing', () => {
-      mount( componentWithTheme( <Index /> ) );
+      mount(
+         componentWithTheme(
+            <Router>
+               <Index />
+            </Router>
+         )
+      );
    } );
 } );
